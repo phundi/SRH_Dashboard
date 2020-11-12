359a93606e6e8720f71b22d9a3515c717fb53cc1
Kenneth Kapundi <kennethkapundi1@gmail.com>
2020-11-12 12:40:41 +0200

    Renamed My Account to Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,15 +15,7 @@ class ApplicationController < ActionController::Base
       @cur_user = User.find(session[:user_id]) rescue (redirect_to '/logout' and return)
     else
       redirect_to "/logout"
-    end
-
-		if @cur_user.blank?
-			@my_account_text = "Login"
-			@my_account_path = "/login"
-		else
-			@my_account_text = "My Account (#{@cur_user.username})"
-			@my_account_path = "/user/view?user_id=#{@cur_user.id}"
-		end
+    end		
   end
 
   def enabled?(groups=[])
