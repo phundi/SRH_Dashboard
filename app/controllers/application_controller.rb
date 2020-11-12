69847d9e672b8ac108649984904561ef39b2d4c5
Kenneth Kapundi <kennethkapundi1@gmail.com>
2020-11-12 10:14:28 +0200

    Added User Management

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  #before_filter :check_user, :except => ['logout', 'login']
+  
+	before_filter :check_user, :except => ['logout', 'login']
   before_filter :check_tab
-  def check_user
+  
+	def check_user
     if params[:active_tab].present?
       session[:active_tab] = params[:active_tab]
     end
@@ -12,8 +14,16 @@ class ApplicationController < ActionController::Base
     if session[:user_id].present?
       @cur_user = User.find(session[:user_id]) rescue (redirect_to '/logout' and return)
     else
-      redirect_to "/logout"
+      #redirect_to "/logout"
     end
+
+		if @cur_user.blank?
+			@my_account_text = "Login"
+			@my_account_path = "/login"
+		else
+			@my_account_text = "My Account (#{@cur_user.username})"
+			@my_account_path = "/user/view?user_id=#{@cur_user.id}"
+		end
   end
 
   def enabled?(groups=[])
@@ -27,6 +37,6 @@ class ApplicationController < ActionController::Base
   end
 
   def check_tab
-	session[:active_tab] = params[:controller]
+		session[:active_tab] = params[:controller]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,30 @@
-puts "Done!!"
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+u = User.create(
+    username: 'admin',
+    password: BCrypt::Password.create('12345'),
+    first_name: 'SRH',
+    last_name: 'Admin',
+    middle_name: 'Moses',
+    email: 'srhadmin@gmail.com',
+    gender: 1,
+    designation: 'Software Developer'
+)
+
+r = Role.create(
+    name: 'Administrator', description: 'Superuser of SRH Dashboard'
+)
+
+Role.create(
+    name: 'Change Agent', description: 'Organisation Unit SRH users'
+)
+
+UserRole.create(
+    user_id: u.id, role_id: r.id
+)
+
